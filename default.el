d0df8a39f2c39733c81261aed55fd7ae23f0d389
Peter Popov <petro.popov@gmail.com>
2013-08-11 00:24:16 +0200

    More features, started useing package.el

--- default.el
+++ default.el
@@ -1,38 +1,50 @@
-(setq x-select-enable-clipboard t)
+;; Emacs config
+;; Peter Popov
 
-;;load path set to folder ~/emacs for .el files
+;; ==============================================================================
+;; Modes
 (add-to-list 'load-path "~/emacs/modes")
-(add-to-list 'load-path "~/emacs/plugins")
-(add-to-list 'load-path "~/tools/cedet-1.1/common")
-;; Apache PIG mode
-(load "pig-mode.el")
-
-;; Google GO mode
-(load "go-mode.el")
-
-;; GTUGS/Global
-(load "gtags.el")
-(setq gtags-select-buffer-single t)
-
-;; CEDET
-(load "cedet.el")
-(require 'cedet)
-(require 'semantic)
-(setq semanticdb-default-save-directory "~/.semantic")
-(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion
-(semantic-load-enable-excessive-code-helpers)
-(global-srecode-minor-mode 1)            ; Enable template insertion menu
-(global-semantic-tag-folding-mode 1)
-(global-semantic-decoration-mode -1)
-(setq-mode-local c++-mode
-                 semanticdb-find-default-throttle
-                 '(project unloaded recursive)) ;; Remove 'system from the throttle
-(setq-mode-local c-mode
-                 semanticdb-find-default-throttle
-                 '(project unloaded recursive)) ;; Remove 'system from the throttle
+(load "pig-mode.el") ; Apache PIG
+(load "go-mode.el") ; Google go language
 
 
-;; Plugins
+;; ==============================================================================
+;; Package managment
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Auto load plugins on start
+;; Taken from here: http://batsov.com/articles/2012/02/19/package-management-in-emacs-the-good-the-bad-and-the-ugly/
+(defvar prelude-packages
+  '(python ggtags yasnippet auto-complete member-function autopair )
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (message "%s: %s" "Installing" p)
+      (package-install p))))
+
+(provide 'prelude-packages)
+
+
+;; ==============================================================================
+;; Plugins set up
+;; ==============================================================================
+;;
+;;IDO
 (require 'ido)
 (ido-mode t)
 (setq
@@ -40,22 +52,41 @@
  ido-enable-flex-matching t         ; enable fuzzy matching
  ido-max-prospects 6                ; don't spam my minibuffer
  ido-confirm-unique-completion t )  ; wait for RET, even with unique completion
-
-;; Show whitespaces
-(require 'show-wspace)
-
 ;;
 ;; nxml mode
-;;
 (add-to-list 'auto-mode-alist
              (cons (concat "\\." (regexp-opt '("xml" "xsd" "rng" "xslt" "svg" "rss") t) "\\'")
                    'nxml-mode))
 (fset 'xml-mode 'nxml-mode)
 (fset 'html-mode 'nxml-mode)
+;;
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+;;
+;; auto complete mod
+;; TURNED OFF BECAUSE OF ISSUE WITH YAS INTERGATION, see:
+;;  http://www.kurup.org/blog/2012/10/15/emacs-autocomplete-stumbles-on-yasnippet/
+;;; should be loaded after yasnippet so that they can work together
+;;(require 'auto-complete-config)
+;;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;;(delq 'ac-source-yasnippet ac-sources)
+;;(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+;;(ac-set-trigger-key "TAB")
+;;(ac-set-trigger-key "<tab>")
+;;
+;; member function
+(require 'member-function)
+(setq mf--source-file-extension "cpp")
+
 
 
 ;; Dont show the GNU splash screen
 (setq inhibit-startup-message t)
+(setq x-select-enable-clipboard t)
 
 ;; turn on visual bell
 (setq visible-bell t)
@@ -65,7 +96,6 @@
 (menu-bar-mode 0)
 ;; Show column number at bottom of screen
 (column-number-mode 1)
-
 
 (set-default 'truncate-lines t)
 (setq font-lock-maximum-decoration t)
@@ -97,8 +127,9 @@
 (setq c-default-style "k&r"
       c-basic-offset 4)
 (setq-default indent-tabs-mode nil)
-(toggle-show-tabs-show-ws)
-(toggle-show-trailing-whitespace-show-ws)
+(setq-default show-trailing-whitespace t)
+;;(toggle-show-tabs-show-ws)
+;;(toggle-show-trailing-whitespace-show-ws)
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
@@ -108,7 +139,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(inhibit-startup-screen t)
  '(initial-buffer-choice nil)
  '(initial-scratch-message nil)
  '(remote-shell-program "bash"))
